467499c86dd7faba03bcd5a010e1aba5d9d39d01
aftisseamel <asensioamel@gmail.com>
2025-04-15 11:52:31 +0200

    update

--- maylisse-app/app/commande/page.tsx
+++ maylisse-app/app/commande/page.tsx
@@ -6,8 +6,11 @@ async function Commande() {
     const supabase = await createClient();
     //const {data} = await supabase.from('deliveryman').select('*').limit(1).single();
 
-    const { data, error } = await supabase.from('order').select('*').single();
-
+    const { data, count, error } = 
+    await supabase.from('article').select('quantity')
+    .gt('quantity', 555);
+    const total = data?.reduce((acc, item) => acc + item.quantity, 0);
+    console.log('Somme des quantités > 555 :', total);
 
     if (error) {
     console.error("Erreur Supabase:", error)
---
+++ maylisse-app/supabase/migrations/20250415095122_remote_schema.sql
@@ -0,0 +1,3 @@
+alter table "public"."article" alter column "quantity" set default '0'::smallint;
+
+
