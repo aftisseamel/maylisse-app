33b861a9eb05dbcb274ae04808027869e5dd2eb6
aftisseamel <bifo@BIFO.localdomain>
2025-06-04 10:35:48 +0200

    change type from ts to js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
